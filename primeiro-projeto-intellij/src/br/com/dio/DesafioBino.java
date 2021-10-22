bd1996e6e3bdb7e63538c0386e2a6fb57826ca68
silvaaleksandro <silvaal@gmail.com>
2021-10-22 19:47:40 -0300

    adicionando todos os arquivos da pasta

--- primeiro-projeto-intellij/src/br/com/dio/DesafioBino.java
+++ primeiro-projeto-intellij/src/br/com/dio/DesafioBino.java
@@ -1,2 +1,48 @@
-package br.com.dio;public class DesafioBino {
+package br.com.dio;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DesafioBino {
+    public static void main(String[] args) throws IOException {
+       theon();
+    }
+
+    public static void theon(){
+        Scanner leitor = new Scanner(System.in);
+        int N = leitor.nextInt();
+        int T , menor = 0, posMenor = 0;
+        for ( int p=1; p<= N; p++) {
+            System.out.println("Posicao:"+p);
+            T = leitor.nextInt();
+            if ( p == 1  ) {
+                posMenor = p;
+                menor = T;
+            } else if (  T < menor  ) {
+                posMenor = p;
+                menor = T;
+            }
+        }
+        System.out.println("Menor: "+posMenor);
+    }
+
+    public static void bino(){
+        Scanner leitor = new Scanner(System.in);
+        while (true) {
+            System.out.println("hora: ");
+            String[] relogio = leitor.nextLine().split(":");
+            int hora = Integer.parseInt(relogio[0]);
+            int min = Integer.parseInt(relogio[1]);
+
+            switch ( hora  ) {
+                case(7): System.out.println("Atraso maximo: " + min); break;
+                case(8): System.out.println("Atraso maximo: " + (60 + min) ); break;
+                case(9): System.out.println("Atraso maximo: " + (120 + min) ); break;
+                default: System.out.println("Atraso maximo: 0");
+            }
+            if(!leitor.hasNext()) break;
+        }
+    }
 }
--- primeiro-projeto-intellij/src/br/com/dio/GravaArqTexto.java
+++ primeiro-projeto-intellij/src/br/com/dio/GravaArqTexto.java
@@ -1,2 +1,38 @@
-package br.com.dio;public class GravaArqTexto {
+package br.com.dio;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class GravaArqTexto {
+    public static void main(String[] args) throws FileNotFoundException {
+        recebeEntrada();
+        System.out.print("Fim");
+    }
+
+    public static void recebeEntrada() throws FileNotFoundException {
+        String aux;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Nome do arquivo: ");
+        String nomeArq = scan.next();
+        PrintWriter escritor = new PrintWriter(nomeArq + ".txt");
+        try{
+            while (true){
+                System.out.print("Hora da Saída: ");
+                aux = scan.next();
+                if(!aux.equals("X")){
+                    escritor.println(aux);
+                }else{
+                    break;
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            escritor.close();
+            System.out.print("Arquivo Fechado:");
+        }
+
+
+    }
 }
